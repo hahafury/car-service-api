ac828f7de866de5c13ba199cc00b4f556f00feaf
hahafury <bladeb071@gmail.com>
2023-09-24 16:41:35 +0300

    :recycle: Refactored base-index-query.dto

--- app/common/base/base-index-query.dto.ts
+++ app/common/base/base-index-query.dto.ts
@@ -1,15 +1,16 @@
 import { IsNumber, IsOptional } from 'class-validator';
 import { Transform } from 'class-transformer';
 import { transformStringToNumber } from '@app/common/transforms';
+import { PAGINATION_DEFAULT_PROPERTIES } from '@app/common/helpers/pagination';
 
 export class BaseIndexQueryDto {
   @Transform(transformStringToNumber)
   @IsOptional()
   @IsNumber()
-  public readonly limit?: number | null;
+  public readonly limit?: number = PAGINATION_DEFAULT_PROPERTIES.LIMIT;
 
   @Transform(transformStringToNumber)
   @IsOptional()
   @IsNumber()
-  public readonly page?: number | null;
+  public readonly page?: number = PAGINATION_DEFAULT_PROPERTIES.LIMIT;
 }
