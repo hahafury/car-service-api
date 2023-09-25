64d3f25a9976c563d2a4fffaa42ce9e181f319f3
Олексанр Чумак <cauua@air-oleksanr.lan>
2023-09-25 09:44:52 +0200

    :recycle: Refactored auth dto

--- app/modules/user/dto/login-user.dto.ts
+++ app/modules/user/dto/login-user.dto.ts
@@ -1,15 +1,27 @@
 import { ApiProperty } from '@nestjs/swagger';
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+  IsStrongPassword,
+} from 'class-validator';
 
 export class LoginUserDto {
   @ApiProperty({
     example: '1234578910',
     required: true,
   })
+  @IsNotEmpty()
+  @IsString()
+  @IsEmail()
   email: string;
 
   @ApiProperty({
     example: '1234578910',
     required: true,
   })
+  @IsNotEmpty()
+  @IsString()
+  @IsStrongPassword()
   password: string;
 }
--- app/modules/user/dto/register-user.dto.ts
+++ app/modules/user/dto/register-user.dto.ts
@@ -1,16 +1,27 @@
 import { ApiProperty } from '@nestjs/swagger';
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsPhoneNumber,
+  IsString,
+  IsStrongPassword,
+} from 'class-validator';
 
 export class RegisterUserDto {
   @ApiProperty({
     example: 'Joe',
     required: true,
   })
+  @IsNotEmpty()
+  @IsString()
   name: string;
 
   @ApiProperty({
     example: 'Johnson',
     required: true,
   })
+  @IsNotEmpty()
+  @IsString()
   surname: string;
 
   // credentials
@@ -18,23 +29,35 @@ export class RegisterUserDto {
     example: 'john.doe@example.com',
     required: true,
   })
+  @IsNotEmpty()
+  @IsString()
+  @IsEmail()
   email: string;
 
   @ApiProperty({
     example: '+384405551234',
     required: true,
   })
+  @IsNotEmpty()
+  @IsString()
+  @IsPhoneNumber()
   phone: string;
 
   @ApiProperty({
     example: 'myStrongPassword',
     required: true,
   })
+  @IsNotEmpty()
+  @IsString()
+  @IsStrongPassword()
   password: string;
 
   @ApiProperty({
     example: 'myStrongPassword',
     required: true,
   })
+  @IsNotEmpty()
+  @IsString()
+  @IsStrongPassword()
   confirmPassword: string;
 }
